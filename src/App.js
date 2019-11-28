89cfe1bd91909f5a117a18d56baf200f69234ebf
kalana <kalanaramesh16@gmail.com>
2019-11-28 11:23:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
 
+//2260467
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
+      <Modal />
+    </>
   );
+
 }
 
-export default App;
+export default App
+
+// npm install --save bootstrap
+// npm install --save react-router-dom
+// npm install --save styled-components   => www.styled-compoenetts.com
